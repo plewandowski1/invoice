8b027073502bf97cd3e375cf34e3caa92e6a1828
Lewandowski <pawel.1.lewandowski.ext@nokia.com>
2018-11-09 10:45:10 +0100

    header template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StandardButtonComponent } from './standard-button/standard-button.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StandardButtonComponent
+    StandardButtonComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
