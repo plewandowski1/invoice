6bdd9298d476e7da62ccd539527d49c57405cb32
Lewandowski <pawel.1.lewandowski.ext@nokia.com>
2018-11-09 13:11:00 +0100

    layout for new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StandardButtonComponent } from './basic-components/standard-button/standard-button.component';
 import { HeaderComponent } from './containers/header/header.component';
+import { StandardInputComponent } from './basic-components/standard-input/standard-input.component';
+import { InvoiceHeaderComponent } from './containers/invoice-header/invoice-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StandardButtonComponent,
-    HeaderComponent
+    HeaderComponent,
+    StandardInputComponent,
+    InvoiceHeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/basic-components/standard-input/standard-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-standard-input',
+  templateUrl: './standard-input.component.html',
+  styleUrls: ['./standard-input.component.scss']
+})
+export class StandardInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
